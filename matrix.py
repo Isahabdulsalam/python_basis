a0fbc0c04bdb7b9e05320075c98e5ef41b95733f
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-02-06 01:21:35 +0100

    Matrix

--- matrix.py
+++ matrix.py
@@ -1,13 +1,13 @@
 row1 = ['😀','😀,','😀']
 row2 = ['😀','😀,','😀']
 row3 = ['😀','😀,','😀']
-maxtrix = [row1, row2, row3]
+matrix = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 
 position=input("Enter the position yoi want to hide your money: ")
 #32
 row_number=int(position[0])
 column_number=int(position[1])
-row_selected=maxtrix[row_number-1]
+row_selected=matrix[row_number-1]
 row_selected[column_number-1]= '👏'
 print(f"{row1}\n{row2}\n{row3}")
